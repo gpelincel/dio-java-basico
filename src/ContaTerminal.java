1bbad78af3ef229339cc8dcd00b669b53f199c78
gpelincel <gabrielpelincel@gmail.com>
2024-10-12 11:55:00 -0300

    feat: Programa concluido

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o seu nome:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o numero da sua conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o numero da sua agencia:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da sua conta:");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Olá "+nome+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
     }
 }
